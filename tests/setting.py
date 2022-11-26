e55affc4cd8b4cd28fca1dcd6bd389e57dc44117
Julai8 <109540430+Julai8@users.noreply.github.com>
2022-11-26 12:26:12 +0300

    Initial commit

---
+++ tests/setting.py
@@ -0,0 +1,9 @@
+
+valid_email = "zxcasdqwe@mail.ru"
+valid_password = "pogoda2"
+
+valid_email = "catdog34@mail.ru"
+valid_password = "pogoda34"
+
+valid_email = "petflyw@mail.ru"
+valid_password = "pogoda1234"
